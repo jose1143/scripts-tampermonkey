aadc3cd571d38a56385797e76569fc1eb443ed27
jose1143 <fake1987ba@gmail.com>
2025-10-10 13:09:56 +0200

    Update lista-blanca-trabajo.js

--- lista-blanca-trabajo.js
+++ lista-blanca-trabajo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bloqueador de Páginas Lista blanca
 // @namespace    http://tampermonkey.net/
-// @version      1.1.5
+// @version      1.1.6
 // @description  Bloquea el acceso a páginas específicas usando expresiones regulares
 // @author       Tu nombre
 // @match        *://*/*
@@ -42,7 +42,8 @@
         /screenful.me/i,
         /cloudflare.com/i,
         /jetbrains.com/i,
-        /lipsum.com/i
+        /lipsum.com/i,
+        /elementor.com/i
     ];
 
     // Mensaje personalizado que se mostrará
