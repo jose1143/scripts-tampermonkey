543e251e02c56004c16714864cbdc3fbe5663bde
jose1143 <fake1987ba@gmail.com>
2025-10-18 11:40:28 +0200

    Update lista-blanca-personal.js

--- lista-blanca-personal.js
+++ lista-blanca-personal.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bloqueador de Páginas Lista blanca
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  Bloquea el acceso a páginas específicas usando expresiones regulares
 // @author       Tu nombre
 // @match        *://*/*
